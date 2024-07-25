0ac95f9c65afde47bfd03f110fb5a530ab72ee3d
Mostafa Ghorab <vetghorab@gmail.com>
2024-07-25 09:47:01 +0300

    added 1 query and 1 mutation to get jobs and create new job

--- resolvers.js
+++ resolvers.js
@@ -1,5 +1,23 @@
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
 export const resolvers = {
   Query: {
-    greeting: () => 'Hello, GraphQL!',
-  }
-}
+    jobs: async () => {
+      const jobs = await prisma.job.findMany();
+      return jobs;
+    },
+  },
+  Mutation: {
+    createJob: async (_, args) => {
+      const { title, description } = args.data;
+      return await prisma.job.create({
+        data: {
+          title,
+          description,
+        },
+      });
+    },
+  },
+};
